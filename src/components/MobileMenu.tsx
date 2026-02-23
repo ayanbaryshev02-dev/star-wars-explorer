5d6b81756715ad2f7929259a961c025541c29a6a
Ayan <ayanbaryshev02@gmail.com>
2026-02-23 15:10:04 +0500

    del excess import

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
 interface MobileMenuProps {
   navItems: Array<{ name: string; section: string; icon: string }>;
